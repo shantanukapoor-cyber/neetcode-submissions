ce2c7acc4a86d9cf9a6643077dc8416901e6ba1c
shantanukapoor-cyber <shantanu.d.kapoor@gmail.com>
2026-05-28 01:15:55 -0700

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        memo = {}
+
+        def maxCost(index):
+            if index >= len(nums):
+                return 0
+            if index in memo:
+                return memo[index]
+            # Pick
+            x = maxCost(index+2) + nums[index]
+            # Skip
+            y = maxCost(index+1)
+            memo[index] = max(x, y)
+            return memo[index]
+
+        return max(maxCost(0),maxCost(1))
